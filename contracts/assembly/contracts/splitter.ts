93ba04cb4ba045b1e58f43827d809f16fb45dea5
Ayoub Amer <ayouub.ameur@gmail.com>
2025-09-13 14:28:23 +0100

    feat: enhance deposit function to fetcheach token percentage and improve storage management using custom persistentMap

--- contracts/assembly/contracts/splitter.ts
+++ contracts/assembly/contracts/splitter.ts
@@ -5,17 +5,22 @@ import {
   generateEvent,
   Storage,
 } from '@massalabs/massa-as-sdk';
-import { Args, stringToBytes } from '@massalabs/as-types';
+import { Args, SafeMath, stringToBytes } from '@massalabs/as-types';
 import { TokenWithPercentage } from './structs/token';
 import { _setOwner } from './lib/ownership-internal';
-import { PersistentMap } from '@massalabs/massa-as-sdk/assembly/collections';
 import { ReentrancyGuard } from './lib/ReentrancyGuard';
 import { wrapMasToWMAS } from './lib/wrapping';
 import { WMAS_TOKEN_ADDRESS } from './storage';
 import { IMRC20 } from './interfaces/IMRC20';
 import { getBalanceEntryCost } from '@massalabs/sc-standards/assembly/contracts/MRC20/MRC20-external';
+import { deserializeStringArray, serializeStringArray } from './lib/utils';
+import { u256 } from 'as-bignum/assembly';
+import { SafeMath256 } from './lib/safeMath';
+import { PersistentMap } from './lib/PersistentMap';
 
-const tokensPercentagesMap = new PersistentMap<String, u64>('tpm');
+const tokensPercentagesMap = new PersistentMap<string, u64>('tpm');
+const allTokensAddressesKey: StaticArray<u8> =
+  stringToBytes('allTokensAddresses');
 
 /**
  * This function is meant to be called only one time: when the contract is deployed.
@@ -37,11 +42,17 @@ export function constructor(binaryArgs: StaticArray<u8>): void {
     .nextString()
     .expect('vault creator address expected');
 
+  const allTokensAddresses = new Array<string>();
+
   // Store the tokens and their percentages in the persistent map
   for (let i = 0; i < tokenWithPercentage.length; i++) {
     const token = tokenWithPercentage[i];
     tokensPercentagesMap.set(token.address.toString(), token.percentage);
+    allTokensAddresses.push(token.address.toString());
   }
+
+  // Store all token addresses in the storage
+  Storage.set(allTokensAddressesKey, serializeStringArray(allTokensAddresses));
 
   // Set the contract owner to the vault creator address
   _setOwner(vaultCreatorAddress);
@@ -74,7 +85,37 @@ export function deposit(binaryArgs: StaticArray<u8>): void {
     );
   }
 
-  // TODO: Distribute the  WMAS amount to the tokens according to their percentages
+  // Distribute the  WMAS amount to the tokens according to their percentages
+
+  // Get all tokens and their corresponding percentages from the persistent map
+  const tokens: string[] = deserializeStringArray(
+    Storage.get(allTokensAddressesKey),
+  );
+
+  for (let i = 0; i < tokens.length; i++) {
+    const tokenAddress = tokens[i];
+
+    assert(
+      tokensPercentagesMap.contains(tokenAddress),
+      'TOKEN_PERC_NOT_FOUND: ' + tokenAddress,
+    );
+    const percentage = tokensPercentagesMap.get(tokenAddress, 0);
+
+    // Calculate the amount to send to each token based on its percentage
+    // tokenAmount  = amount * percentage / 100
+    const tokenAmount = SafeMath256.div(
+      SafeMath256.mul(amount, u256.fromU64(percentage)),
+      u256.fromU64(100),
+    );
+
+    // TODO: the actual swap on eaglefi DEX
+    // const poolAddress = ;
+
+    // For now, just log the token address and the amount to swap
+    generateEvent(
+      'Token: ' + tokenAddress + ', Amount to swap: ' + tokenAmount.toString(),
+    );
+  }
 
   // End Reentrancy Guard
   ReentrancyGuard.endNonReentrant();
