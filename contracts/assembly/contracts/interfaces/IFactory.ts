92c3d2d51d99d2124d8ec64cc948a41c34e851f1
Ayoub Amer <ayouub.ameur@gmail.com>
2025-09-13 14:35:25 +0100

    feat: implement IFactory interface with methods for managing token pools and vaults

---
+++ contracts/assembly/contracts/interfaces/IFactory.ts
@@ -0,0 +1,45 @@
+import { Address, call } from '@massalabs/massa-as-sdk';
+import { TokenWithPercentage } from '../structs/token';
+import { Args, bytesToString } from '@massalabs/as-types';
+
+export class IFactory {
+  _origin: Address;
+
+  constructor(origin: Address) {
+    this._origin = origin;
+  }
+
+  createSplitterVault(
+    tokensWithPercentage: TokenWithPercentage[],
+    coins: u64 = 0,
+  ): void {
+    const args = new Args();
+
+    args.addSerializableObjectArray(tokensWithPercentage);
+
+    call(this._origin, 'createSplitterVault', args, coins);
+  }
+
+  setTokenPoolAddress(
+    tokenAddress: string,
+    poolAddress: string,
+    coins: u64 = 0,
+  ): void {
+    const args = new Args();
+
+    args.add(tokenAddress);
+    args.add(poolAddress);
+
+    call(this._origin, 'setTokenPoolAddress', args, coins);
+  }
+
+  getTokenPoolAddress(tokenAddress: string): string {
+    const args = new Args();
+
+    args.add(tokenAddress);
+
+    const result = call(this._origin, 'getTokenPoolAddress', args, 0);
+
+    return bytesToString(result);
+  }
+}
