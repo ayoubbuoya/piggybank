d7dd09486f860a553221541f5a0a439c0bc41b44
Ayoub Amer <ayouub.ameur@gmail.com>
2025-09-13 14:21:29 +0100

    feat: add utility functions for serializing and deserializing string arrays

---
+++ contracts/assembly/contracts/lib/utils.ts
@@ -0,0 +1,19 @@
+import { Args } from "@massalabs/as-types";
+
+/**
+ * Serializes an array of strings into a static array of bytes.
+ * @param arr - Array of strings to serialize.
+ * @returns StaticArray<u8> - Serialized array of bytes.
+ */
+export function serializeStringArray(arr: string[]): StaticArray<u8> {
+  return new Args().add(arr).serialize();
+}
+
+/**
+ * Deserializes a static array of bytes into an array of strings.
+ * @param arr - StaticArray<u8> to deserialize.
+ * @returns Array<string> - Deserialized array of strings.
+ */
+export function deserializeStringArray(arr: StaticArray<u8>): string[] {
+  return new Args(arr).nextStringArray().unwrapOrDefault();
+}
