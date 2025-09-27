4ac0d0cc00f1300c9e5d23353dfb3689d04cad9b
Ayoub Amer <ayouub.ameur@gmail.com>
2025-09-27 11:50:38 +0100

    feat: create eaglefi pools

--- contracts/notes.md
+++ contracts/notes.md
@@ -2,3 +2,5 @@
 
 - EagleFi Factory: AS12AhgNHNydcESphMopqWzcRbwZKfeGpvH17vh9xejnEySDWiagC
 - EagleFi Swap Router: AS1Kf2KVdYghv9PeVcgQKVBpuVAqdvfwwMbGuffByxJbSMLqLvVo
+- EagleFi USDC-WMAS Pool: AS1p6ULD2dWxJ7G1U3D3dX95jHwgfXieRnLFRNRr4Hfq7XvA1qZf
+- EagleFi WETH-WMAS Pool: AS184uE7Eq11Sg3KeQBD7jV9DkC75T3U5P6UEU1WEEZ7FHiesjsh
---
+++ contracts/src/create-pool-with-liquidity.ts
@@ -0,0 +1,145 @@
+import {
+  Account,
+  Args,
+  BUILDNET_TOKENS,
+  Mas,
+  SmartContract,
+  Web3Provider,
+} from '@massalabs/massa-web3';
+import { getScByteCode } from './utils';
+import {
+  createNewPoolWithLiquidity,
+  getPool,
+  setSwapRouterAddress,
+} from '../calls/registry';
+import { getPoolReserves, increaseAllownace } from '../calls/basicPool';
+
+const account = await Account.fromEnv();
+const provider = Web3Provider.buildnet(account);
+const wmasAddress = BUILDNET_TOKENS.WMAS;
+const USDCAddress = 'AS12N76WPYB3QNYKGhV2jZuQs1djdhNJLQgnm7m52pHWecvvj1fCQ';
+const WETHAddress = 'AS12rcqHGQ3bPPhnjBZsYiANv9TZxYp96M7r49iTMUrX8XCJQ8Wrk';
+
+// Create list of pools with liquidity
+const registryAddress = 'AS12AhgNHNydcESphMopqWzcRbwZKfeGpvH17vh9xejnEySDWiagC';
+const registryContract = new SmartContract(provider, registryAddress);
+
+// fIRST usdc - wmas pool
+async function createUSDCWMASPool() {
+  const aTokenAddress = USDCAddress;
+  const bTokenAddress = wmasAddress;
+  const aAmount = 40;
+  const bAmount = 10_000;
+  const inputFeeRate = 0.3 * 10_000;
+
+  //  Increase allownace for both tokens
+  await increaseAllownace(
+    aTokenAddress,
+    registryContract.address,
+    aAmount,
+    provider,
+  );
+
+  await increaseAllownace(
+    bTokenAddress,
+    registryContract.address,
+    bAmount,
+    provider,
+  );
+
+  //  Create a new pool with liquidity
+  await createNewPoolWithLiquidity(
+    registryContract,
+    aTokenAddress,
+    bTokenAddress,
+    aAmount,
+    bAmount,
+    0,
+    0,
+    inputFeeRate,
+    false, // isNativeMas
+    6, // usdc decimals
+    9, // wmas decimals
+  );
+
+  const pool = await getPool(
+    registryContract,
+    aTokenAddress,
+    bTokenAddress,
+    inputFeeRate,
+  );
+
+  console.log('USDC-WMAS Pool Address:', pool.poolAddress);
+
+  const poolContract = new SmartContract(provider, pool.poolAddress);
+
+  //  get pool reserves
+  const [aReserve, bReserve] = await getPoolReserves(poolContract);
+
+  console.log('USDC-WMAS Pool Reserves:', {
+    aReserve,
+    bReserve,
+  });
+}
+
+// Second WETH - wmas pool
+async function createWETHWMASPool() {
+  const aTokenAddress = WETHAddress;
+  const bTokenAddress = wmasAddress;
+  const aAmount = 1;
+  const bAmount = 10_000;
+  const inputFeeRate = 0.3 * 10_000;
+
+  //  Increase allownace for both tokens
+  await increaseAllownace(
+    aTokenAddress,
+    registryContract.address,
+    aAmount,
+    provider,
+  );
+
+  await increaseAllownace(
+    bTokenAddress,
+    registryContract.address,
+    bAmount,
+    provider,
+  );
+
+  //  Create a new pool with liquidity
+  await createNewPoolWithLiquidity(
+    registryContract,
+    aTokenAddress,
+    bTokenAddress,
+    aAmount,
+    bAmount,
+    0,
+    0,
+    inputFeeRate,
+    false, // isNativeMas
+    18, // weth decimals
+    9, // wmas decimals
+  );
+
+  const pool = await getPool(
+    registryContract,
+    aTokenAddress,
+    bTokenAddress,
+    inputFeeRate,
+  );
+
+  console.log('WETH-WMAS Pool Address:', pool.poolAddress);
+
+  const poolContract = new SmartContract(provider, pool.poolAddress);
+
+  //  get pool reserves
+  const [aReserve, bReserve] = await getPoolReserves(poolContract);
+
+  console.log('WETH-WMAS Pool Reserves:', {
+    aReserve,
+    bReserve,
+  });
+}
+
+await createWETHWMASPool();
+
+console.log('All pools created successfully');
