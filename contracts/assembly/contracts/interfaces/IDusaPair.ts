850a5dc4be8ff6aae2b33afa5dafaddd89d58373
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-06 22:23:07 +0100

    feat: add IDusaPair interface for token pair management

---
+++ contracts/assembly/contracts/interfaces/IDusaPair.ts
@@ -0,0 +1,31 @@
+import { Args, stringToBytes } from '@massalabs/as-types';
+import { Address, call, Storage } from '@massalabs/massa-as-sdk';
+import { IMRC20 } from './IMRC20';
+
+/// @dev The fee parameters that are used to calculate fees
+export const FEES_PARAMETERS = stringToBytes('FEES_PARAMETERS');
+/// @notice The token that is used as the base currency for the pair
+export const TOKEN_X = 'TOKEN_X';
+/// @notice The token that is used as the quote currency for the pair
+export const TOKEN_Y = 'TOKEN_Y';
+
+export class IDusaPair {
+  _origin: Address;
+
+  /**
+   * Wraps a smart contract exposing standard token FFI.
+   *
+   * @param {Address} at - Address of the smart contract.
+   */
+  constructor(at: Address) {
+    this._origin = at;
+  }
+
+  getTokenX(): IMRC20 {
+    return new IMRC20(new Address(Storage.getOf(this._origin, TOKEN_X)));
+  }
+
+  getTokenY(): IMRC20 {
+    return new IMRC20(new Address(Storage.getOf(this._origin, TOKEN_Y)));
+  }
+}
