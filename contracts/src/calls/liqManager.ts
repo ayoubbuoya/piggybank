b62092555e38c4475625695950a8cda4e166717c
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-08 13:47:42 +0100

    feat: add liquidity management functions and tests for liqManager

--- contracts/src/calls/liqManager.ts
+++ contracts/src/calls/liqManager.ts
@@ -1,9 +1,16 @@
-import { Args, Mas, SmartContract, Web3Provider } from '@massalabs/massa-web3';
+import {
+  Args,
+  Mas,
+  OperationStatus,
+  parseUnits,
+  SmartContract,
+  Web3Provider,
+} from '@massalabs/massa-web3';
 import { getScByteCode } from '../utils';
 
 export async function deployLiqManager(
   provider: Web3Provider,
-  poolAddress: string = 'AS1p6ULD2dWxJ7G1U3D3dX95jHwgfXieRnLFRNRr4Hfq7XvA1qZf',
+  poolAddress: string = 'AS112Wdy9pM4fvLNLHQXyf7uam9waMPdG5ekr4vxCyQHPkrMMPPY',
   intervalsMs: number = 60000,
 ): Promise<SmartContract> {
   console.log('Deploying liq manager contract...');
@@ -19,11 +26,44 @@ export async function deployLiqManager(
     byteCode,
     constructorArgs,
     {
-      coins: Mas.fromString('0.1'),
+      coins: Mas.fromString('0.3'),
     },
   );
 
   console.log('Liq Manager Contract deployed at:', contract.address);
 
   return contract;
+}
+
+export async function addLiquidity(
+  liqManagerContract: SmartContract,
+  binsRange: number,
+  amountX: string,
+  amountY: string,
+  xDecimals: number = 9,
+  yDecimals: number = 6,
+) {
+  console.log('Adding liquidity...');
+
+  const args = new Args()
+    .addU64(BigInt(binsRange))
+    .addU256(parseUnits(amountX, xDecimals))
+    .addU256(parseUnits(amountY, yDecimals))
+    .serialize();
+
+  const tx = await liqManagerContract.call('addLiquidity', args, {
+    coins: Mas.fromString('0.01'),
+  });
+
+  const status = await tx.waitSpeculativeExecution();
+
+  if (status === OperationStatus.SpeculativeSuccess) {
+    console.log('Liquidity added successfully');
+  } else {
+    console.log('Status:', status);
+    // Show speculative events for debugging
+    const spec_events = await tx.getSpeculativeEvents();
+    console.log('Speculative events:', spec_events);
+    throw new Error('Failed to add liquidity');
+  }
 }
