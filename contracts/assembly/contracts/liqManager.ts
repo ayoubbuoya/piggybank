674f48590eb5a8160b7ec55cc3d58a7f8e1e2c71
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-13 22:41:33 +0100

    feat: add shares calculation in deposit

--- contracts/assembly/contracts/liqManager.ts
+++ contracts/assembly/contracts/liqManager.ts
@@ -1,6 +1,8 @@
 import {
   Args,
+  bytesToU256,
   bytesToU64,
+  NoArg,
   stringToBytes,
   u64ToBytes,
   u8toByte,
@@ -21,8 +23,15 @@ import { PairInformation } from './structs/dusa/PairInfo';
 import { LiquidityParameters } from './structs/dusa/LiquidityParameters';
 import { SafeMath256 } from './lib/safeMath';
 import { PRECISION } from './lib/constants';
-import { arrayToString } from './lib/utils';
+import { arrayToString, u256ToString } from './lib/utils';
 import { FeeParameters } from './structs/dusa/FeeParameters';
+import { IDusaRouter } from './interfaces/IdusaRouter';
+import { getBalanceEntryCost } from '@massalabs/sc-standards/assembly/contracts/MRC20/MRC20-external';
+import {
+  mrc20Constructor,
+  totalSupply as _totalSupply,
+} from '@massalabs/sc-standards/assembly/contracts/MRC20/MRC20';
+import { _mint } from '@massalabs/sc-standards/assembly/contracts/MRC20/mintable/mint-internal';
 
 // Storage Keys
 export const PAIR_ADDRESS_KEY = 'pa';
@@ -32,6 +41,8 @@ export const PAIR_TOKEN_Y_KEY = 'pty';
 export const PAIR_TOKEN_X_DECIMALS_KEY: StaticArray<u8> = stringToBytes('ptxd');
 export const PAIR_TOKEN_Y_DECIMALS_KEY: StaticArray<u8> = stringToBytes('ptyd');
 export const INTERVALS_MS_KEY: StaticArray<u8> = stringToBytes('ims');
+export const ROUTER_ADDRESS_KEY = 'ra';
+export const USER_SHARE_KEY_PREFIX: StaticArray<u8> = stringToBytes('us_'); // + user address
 
 export function constructor(binaryArgs: StaticArray<u8>): void {
   // This line is important. It ensures that this function can't be called in the future.
@@ -43,6 +54,10 @@ export function constructor(binaryArgs: StaticArray<u8>): void {
   const pairAddress = args
     .nextString()
     .expect('pairAddress argument is missing');
+
+  const routerAddress = args
+    .nextString()
+    .expect('routerAddress argument is missing');
 
   const intervalsMs = args.nextU64().expect('intervalsMs argument is missing');
 
@@ -81,6 +96,12 @@ export function constructor(binaryArgs: StaticArray<u8>): void {
   // Store the intervals in milliseconds
   Storage.set(INTERVALS_MS_KEY, u64ToBytes(intervalsMs));
 
+  // Store the router address
+  Storage.set(ROUTER_ADDRESS_KEY, routerAddress);
+
+  // Intialize Vault LP Positions Token
+  mrc20Constructor('PiggyBank LP Positions', 'PBLP', 18, u256.Zero);
+
   // Initialize the reentrancy guard
   ReentrancyGuard.__ReentrancyGuard_init();
 }
@@ -90,8 +111,92 @@ export function deposit(binaryArgs: StaticArray<u8>): void {
 
   const args = new Args(binaryArgs);
 
-  const amountX = args.nextU256().expect('amountX argument is missing');
-  const amountY = args.nextU256().expect('amountY argument is missing');
+  const amountX: u256 = args.nextU256().expect('amountX argument is missing');
+  const amountY: u256 = args.nextU256().expect('amountY argument is missing');
+
+  // Transfer tokens to the contract
+  const tokenXAddress = Storage.get(PAIR_TOKEN_X_KEY);
+  const tokenYAddress = Storage.get(PAIR_TOKEN_Y_KEY);
+  const tokenX: IMRC20 = new IMRC20(new Address(tokenXAddress));
+  const tokenY: IMRC20 = new IMRC20(new Address(tokenYAddress));
+
+  const callerAddress = Context.caller();
+  const currentContractAddress = Context.callee();
+
+  // Transfer token X
+  tokenX.transferFrom(
+    callerAddress,
+    currentContractAddress,
+    amountX,
+    getBalanceEntryCost(tokenXAddress, currentContractAddress.toString()),
+  );
+
+  // Transfer token Y
+  tokenY.transferFrom(
+    callerAddress,
+    currentContractAddress,
+    amountY,
+    getBalanceEntryCost(tokenYAddress, currentContractAddress.toString()),
+  );
+
+  // Claulcate User share and store it
+  const spotPrice = _fetchPairSpotPrice();
+
+  const depositXPricedInTokenY = SafeMath256.div(
+    SafeMath256.mul(amountX, spotPrice),
+    PRECISION,
+  );
+
+  // shares = depositXPricedInTokenY + amountY
+  let shares: u256 = SafeMath256.add(depositXPricedInTokenY, amountY);
+
+  // Get the total supply
+  const totalSupply: u256 = bytesToU256(_totalSupply(new Args().serialize()));
+
+  // Get toal amounts in the vault
+  const totals = _getVaultTotalTokensAmounts();
+
+  const totalX = totals[0];
+  const totalY = totals[1];
+
+  if (totalSupply > u256.Zero) {
+    const vaultXPricedInTokenY = SafeMath256.div(
+      SafeMath256.mul(totalX, spotPrice),
+      PRECISION,
+    );
+    shares = SafeMath256.div(
+      SafeMath256.mul(shares, totalSupply),
+      SafeMath256.add(vaultXPricedInTokenY, totalY),
+    );
+  }
+
+  // Mint LP Tokens to the user representing their share in the vault
+  const userShareStorageKey = userShareKey(callerAddress.toString());
+
+  const isPreviouslyDeposited = Storage.has(userShareStorageKey);
+
+  let totalUserShares: u256 = shares;
+
+  if (isPreviouslyDeposited) {
+    const previousShares: u256 = bytesToU256(Storage.get(userShareStorageKey));
+
+    totalUserShares = SafeMath256.add(previousShares, shares);
+  }
+
+  _mint(new Args().add(callerAddress.toString()).add(shares).serialize());
+
+  // TODO: Increase liquidity in the Current Position instead of just holding tokens until the next deffered call checkpoint
+
+  generateEvent(
+    createEvent('TOKENS_DEPOSITED', [
+      Context.caller().toString(),
+      u256ToString(amountX),
+      u256ToString(amountY),
+      u256ToString(shares),
+      u256ToString(totalUserShares),
+      u256ToString(totalSupply),
+    ]),
+  );
 
   // End the non-reentrant block
   ReentrancyGuard.endNonReentrant();
@@ -158,16 +263,6 @@ export function addLiquidity(binaryArgs: StaticArray<u8>): void {
     'distributionsY and deltaIds length mismatch',
   );
 
-  // Log distributions and deltaIds
-
-  generateEvent(
-    createEvent('DISTRIBUTIONS_AND_DELTAS', [
-      arrayToString(distributionsX),
-      arrayToString(distributionsY),
-      deltaIds.toString(),
-    ]),
-  );
-
   // Get pair tokens and bin step
   const tokenX: IMRC20 = pair.getTokenX();
   const tokenY: IMRC20 = pair.getTokenY();
@@ -193,8 +288,29 @@ export function addLiquidity(binaryArgs: StaticArray<u8>): void {
     u64.MAX_VALUE,
   );
 
+  const router = new IDusaRouter(new Address(Storage.get(ROUTER_ADDRESS_KEY)));
 
-  
+  router.addLiquidity(liqParams, 20000000);
+
+  generateEvent(
+    createEvent('LIQUIDITY_ADDED', [
+      Context.caller().toString(),
+      amountX.toString(),
+      amountY.toString(),
+    ]),
+  );
+
+  // End the non-reentrant block
+  ReentrancyGuard.endNonReentrant();
+}
+
+export function withdraw(binaryArgs: StaticArray<u8>): void {
+  ReentrancyGuard.nonReentrant();
+
+  const args = new Args(binaryArgs);
+
+  const toAddress = args.nextString().expect('toAddress argument is missing');
+  const amount = args.nextU256().expect('amount argument is missing');
 
   // End the non-reentrant block
   ReentrancyGuard.endNonReentrant();
@@ -227,3 +343,37 @@ export function getTokenYDecimals(): u8 {
   const bytes = Storage.get(PAIR_TOKEN_Y_DECIMALS_KEY);
   return bytes[0];
 }
+
+export function _fetchPairSpotPrice(): u256 {
+  return new u256(1);
+}
+
+export function _getVaultTotalTokensAmounts(): u256[] {
+  const tokenXAddress = Storage.get(PAIR_TOKEN_X_KEY);
+  const tokenYAddress = Storage.get(PAIR_TOKEN_Y_KEY);
+
+  const tokenX: IMRC20 = new IMRC20(new Address(tokenXAddress));
+  const tokenY: IMRC20 = new IMRC20(new Address(tokenYAddress));
+
+  const currentContractAddress = Context.callee();
+
+  const balanceX = tokenX.balanceOf(currentContractAddress);
+  const balanceY = tokenY.balanceOf(currentContractAddress);
+
+  // TODO: get amounts locked in the pair liquidity positions AND add them to the balances
+
+  return new Array<u256>(2).fill(u256.Zero).map((_, i) => {
+    if (i == 0) {
+      return balanceX;
+    } else {
+      return balanceY;
+    }
+  });
+}
+
+export function userShareKey(userAddress: string): StaticArray<u8> {
+  return USER_SHARE_KEY_PREFIX.concat(stringToBytes(userAddress));
+}
+
+// Re-export MRC721 enumerable functions
+export * from '@massalabs/sc-standards/assembly/contracts/MRC721/enumerable';
